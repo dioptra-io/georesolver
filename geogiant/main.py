246118f52d7e5e23456fe4113641ffe573778058
hrimlinger <hugorimlinger4@gmail.com>
2024-06-25 13:22:44 +0000

    main v1

--- geogiant/main.py
+++ geogiant/main.py
@@ -1,7 +1,9 @@
 import sys
 import typer
+import time
 import asyncio
 
+from uuid import uuid4
 from tqdm import tqdm
 from pyasn import pyasn
 from loguru import logger
@@ -13,11 +15,27 @@ from geogiant.evaluation.ecs_geoloc_eval import (
     select_one_vp_per_as_city,
 )
 from geogiant.hostname_init import resolve_vps_subnet
-from geogiant.ecs_vp_selection.scores import get_scores
+from geogiant.ecs_vp_selection.scores import get_scores, TargetScores
+from geogiant.prober import RIPEAtlasProber
 from geogiant.common.utils import get_parsed_vps
-from geogiant.common.files_utils import create_tmp_json_file, load_csv, load_json
-from geogiant.common.queries import get_min_rtt_per_vp, load_vps
-from geogiant.common.ip_addresses_utils import get_prefix_from_ip
+from geogiant.common.files_utils import (
+    create_tmp_json_file,
+    load_csv,
+    load_json,
+    load_pickle,
+)
+from geogiant.common.queries import (
+    get_min_rtt_per_vp,
+    load_vps,
+    get_subnets_mapping,
+    insert_scores,
+    load_target_scores,
+    retrieve_pings,
+    load_target_geoloc,
+    load_cached_targets,
+    insert_geoloc,
+)
+from geogiant.common.ip_addresses_utils import get_prefix_from_ip, route_view_bgp_prefix
 from geogiant.common.settings import PathSettings, ClickhouseSettings, ConstantSettings
 
 path_settings = PathSettings()
@@ -25,34 +43,57 @@ clickhouse_settings = ClickhouseSettings()
 constant_settings = ConstantSettings()
 
 
-async def calculate_scores(hostnames: list[str]) -> None:
+def parsed_score(score_per_granularity: dict, answer_granularity: str) -> list[str]:
+    """parse score function of granularity and return list for insert"""
+    score_data = []
+    for subnet, score_per_metric in score_per_granularity.items():
+        for metric, vps_subnet_score in score_per_metric.items():
+            for vp_subnet, score in vps_subnet_score:
+                score_data.append(
+                    f"{subnet},\
+                    {vp_subnet},\
+                    {metric},\
+                    {answer_granularity},\
+                    {score}"
+                )
 
-    selected_hostnames_per_cdn, selected_hostnames = load_hostnames()
-    score_subnets = load_subnets()
+    return score_data
+
+
+async def calculate_scores(target_subnets: list[str], hostnames: list[str]) -> None:
+    """calculate score and insert"""
 
     batch_size = 1_000  # score calculation for batch of 1000 subnets
-    for i in range(0, len(score_subnets), batch_size):
+    for i in range(0, len(target_subnets), batch_size):
         output_path = (
             path_settings.RESULTS_PATH
-            / f"internet_scale_evaluation/score__internet_scale_{uuid4()}.pickle"
+            / f"internet_scale_evaluation/score__geoResolver_5468f310-c23e-45f2-b19b-c86b796a043e.pickle"
         )
-        subnets = score_subnets[i : i + batch_size]
+        subnets = target_subnets[i : i + batch_size]
         subnet_tmp_path = create_tmp_json_file(subnets)
 
         score_config = {
             "targets_subnet_path": subnet_tmp_path,
-            "vps_subnet_path": VPS_SUBNET_PATH,
-            "hostname_per_cdn": selected_hostnames_per_cdn,
-            "selected_hostnames": selected_hostnames,
-            "targets_ecs_table": ECS_TABLE,
-            "vps_ecs_table": VPS_ECS_TABLE,
+            "vps_table": clickhouse_settings.VPS_FILTERED_TABLE,
+            "selected_hostnames": hostnames,
+            "targets_ecs_table": clickhouse_settings.ECS_TARGET_TABLE,
+            "vps_ecs_table": clickhouse_settings.ECS_VPS_TABLE,
             "hostname_selection": "max_bgp_prefix",
             "score_metric": ["jaccard"],
             "answer_granularities": ["answer_subnets"],
             "output_path": output_path,
         }
 
-        get_scores(score_config)
+        scores: TargetScores = get_scores(score_config)
+        # scores: TargetScores = load_pickle(output_path)
+        target_score_subnet = scores.score_answer_subnets
+
+        score_data = parsed_score(target_score_subnet, "answer_subnets")
+
+        await insert_scores(
+            csv_data=score_data,
+            output_table=clickhouse_settings.SCORE_TARGET_TABLE,
+        )
 
         subnet_tmp_path.unlink()
 
@@ -71,27 +112,37 @@ def get_geo_resolver_schedule(
         target_scores = subnet_scores[target_subnet]
 
         if not target_scores:
-            logger.error(f"{target_subnet} does not have score")
-        for _, target_score in target_scores.items():
+            raise RuntimeError(f"{target_subnet} does not have score")
 
-            # get vps, function of their subnet ecs score
-            ecs_vps = get_ecs_vps(
-                target_subnet, target_score, vps_per_subnet, last_mile_delay, 5_00
-            )
+        # get vps, function of their subnet ecs score
+        ecs_vps = get_ecs_vps(
+            target_subnet, target_scores, vps_per_subnet, last_mile_delay, 5_00
+        )
 
-            # remove vps that have a high last mile delay
-            ecs_vps = filter_vps_last_mile_delay(ecs_vps, last_mile_delay, 2)
+        # remove vps that have a high last mile delay
+        ecs_vps = filter_vps_last_mile_delay(ecs_vps, last_mile_delay, 2)
 
-            ecs_vps = select_one_vp_per_as_city(
-                ecs_vps, vps_coordinates, last_mile_delay
-            )[: constant_settings.PROBING_BUDGET]
+        ecs_vps = select_one_vp_per_as_city(ecs_vps, vps_coordinates, last_mile_delay)[
+            : constant_settings.PROBING_BUDGET
+        ]
 
-            target_schedule[target] = [vp_addr for vp_addr, _ in ecs_vps]
+        target_schedule[target] = [vp_addr for vp_addr, _ in ecs_vps]
 
     return target_schedule
 
 
-def get_measurement_schedule() -> dict[list]:
+def load_targets_from_score(
+    subnet_scores: dict[list], addr_per_subnet: dict
+) -> list[str]:
+    targets = []
+    for target_subnet in subnet_scores:
+        target_addr = addr_per_subnet[target_subnet][0]
+        targets.append(target_addr)
+
+    return targets
+
+
+def get_measurement_schedule(targets: list[str], subnets: list[str]) -> dict[list]:
     """calculate distance error and latency for each score"""
     asndb = pyasn(str(path_settings.RIB_TABLE))
     vps = load_vps(clickhouse_settings.VPS_FILTERED)
@@ -101,10 +152,9 @@ def get_measurement_schedule() -> dict[list]:
         clickhouse_settings.TRACEROUTES_LAST_MILE_DELAY
     )
 
-    internet_scale_dataset = load_json(path_settings.USER_HITLIST_FILE)
-    # TODO: calculate score + table
-    subnet_scores = load_subnet_scores()
-    targets = load_targets_from_score(subnet_scores, internet_scale_dataset)
+    subnet_scores = load_target_scores(
+        score_table=clickhouse_settings.SCORE_TARGET_TABLE, subnets=subnets
+    )
 
     logger.info(f"Targets in schedule:: {len(targets)}")
 
@@ -147,16 +197,146 @@ async def ecs_mapping(subnets: list[str], hostnames: list[str], ecs_table: str) 
         subnet_tmp_file_path.unlink()
 
 
-async def print_name(name: str) -> None:
-    logger.info(f"Hello {name}")
+async def filter_ecs_subnets(subnets: list[str]) -> list[str]:
+    """retrieve all subnets for which ECS resolution was done"""
+    subnet_mapping = []
+    subnet_mapping = get_subnets_mapping(
+        dns_table=clickhouse_settings.ECS_TARGET_TABLE, subnets=subnets
+    )
+
+    cached_subnets = [subnet for subnet in subnet_mapping]
+    filtered_subnets = set(subnets).difference(set(cached_subnets))
+
+    return list(filtered_subnets)
+
+
+async def filter_score_subnets(subnets: list[str]) -> list[str]:
+    """filter and return subnets for which score calculation is yet to be done"""
+    subnet_score = load_target_scores(
+        score_table=clickhouse_settings.SCORE_TARGET_TABLE, subnets=subnets
+    )
+    cached_subnets = [subnet for subnet in subnet_score]
+    filtered_subnets = set(subnets).difference(set(cached_subnets))
+
+    return list(filtered_subnets)
+
+
+def filter_targets(targets: list[str]) -> list[str]:
+    """return targets for which pings were not made"""
+    cached_targets = load_cached_targets(clickhouse_settings.PING_TARGET_TABLE)
+    filtered_targets = set(targets).difference(set(cached_targets))
+
+    return list(filtered_targets)
+
+
+async def ping_targets(measurement_schedule: list, wait_time: int = 60 * 20) -> None:
+    """perform ping geolocation"""
+    if measurement_schedule:
+
+        batch_size = 1_000
+        for i in range(0, len(measurement_schedule), batch_size):
+
+            batch_schedule = measurement_schedule[i : i + batch_size]
+
+            logger.debug(
+                f"Pings schedule (batch {(i + 1) // batch_size}/{len(measurement_schedule) // batch_size})"
+            )
+
+            output_path = await RIPEAtlasProber(
+                probing_type="ping", probing_tag="ping_targets"
+            ).main(batch_schedule)
+
+            measurement_config = load_json(output_path)
+            measurement_ids = measurement_config["ids"]
+
+            # TODO: wait for batch of measurement to be done instead of wait time
+            time.sleep(60 * 5)
+
+            await insert_measurements(
+                measurement_ids=measurement_ids,
+                ping_table=clickhouse_settings.PING_TARGET_TABLE,
+            )
+
+            output_path.unlink()
+
+
+async def insert_measurements(measurement_ids: list[int], ping_table: str) -> None:
+    logger.info(f"{len(measurement_ids)} measurements to insert")
+
+    logger.info(f"Retreiving results for {len(measurement_ids)} measurements")
+
+    batch_size = 100
+    for i in range(0, len(measurement_ids), batch_size):
+        ids = measurement_ids[i : i + batch_size]
+        await retrieve_pings(ids, ping_table)
+
+
+async def insert_geoloc_from_pings(targets: list[str]) -> None:
+    """insert all geoloc in clickhouse"""
+    asndb = pyasn(str(path_settings.RIB_TABLE))
+    vps = load_vps(clickhouse_settings.VPS_FILTERED)
+    removed_vps = load_json(path_settings.REMOVED_VPS)
+    _, vps_coordinates = get_parsed_vps(vps, asndb, removed_vps)
+
+    target_geoloc = load_target_geoloc(
+        table_name=clickhouse_settings.PING_TARGET_TABLE, targets=targets
+    )
+
+    # parse
+    csv_data = []
+    for target_addr, shortest_ping_data in target_geoloc.items():
+        target_subnet = get_prefix_from_ip(target_addr)
+        target_asn, target_bgp_prefix = route_view_bgp_prefix(target_addr, asndb)
+        if not target_bgp_prefix or not target_asn:
+            target_bgp_prefix = "Unknown"
+            target_asn = -1
+
+        vp_addr = shortest_ping_data[0]
+        vp_subnet = get_prefix_from_ip(vp_addr)
+        vp_asn, vp_bgp_prefix = route_view_bgp_prefix(vp_addr, asndb)
+
+        if not vp_bgp_prefix or not vp_asn:
+            vp_bgp_prefix = "Unknown"
+            vp_asn = -1
+
+        lat, lon, country_code, asn = vps_coordinates[vp_addr]
+        msm_id = shortest_ping_data[1]
+        min_rtt = shortest_ping_data[2]
+
+        csv_data.append(
+            f"{target_addr},\
+            {target_subnet},\
+            {target_bgp_prefix},\
+            {target_asn},\
+            {lat},\
+            {lon},\
+            {country_code},\
+            {vp_addr},\
+            {vp_subnet},\
+            {vp_bgp_prefix},\
+            {vp_asn},\
+            {min_rtt},\
+            {msm_id}"
+        )
+
+    # insert geoloc
+    await insert_geoloc(
+        csv_data=csv_data,
+        output_table=clickhouse_settings.GEOLOC_TARGET_TABLE,
+    )
+
+    return csv_data
 
 
 async def geolocate(
-    target_file: Path, hostname_file: Path = None, verbose: bool = False
+    target_file: Path,
+    hostname_file: Path = None,
+    verbose: bool = False,
+    output_file: Path = None,
 ) -> list[tuple]:
     "main function of GeoResolver, get IP addresses and perform geolocation"
-    ip_addrs = load_csv(target_file)
-    subnets = [get_prefix_from_ip(ip) for ip in ip_addrs]
+    targets = load_csv(target_file)
+    subnets = [get_prefix_from_ip(ip) for ip in targets]
 
     if verbose:
         logger.remove()
@@ -170,17 +350,55 @@ async def geolocate(
     else:
         hostnames = load_csv(hostname_file)
 
-    # 1. checking phase
-    # TODO
+    # 1. checking phase: 1) geoloc, 2) ECS mapping, 3) score check
+    # TODO geolocation exists check
 
-    # 2. perform ECS queries
-    await ecs_mapping(
-        subnets=subnets,
-        hostnames=hostnames,
-        ecs_table=clickhouse_settings.ECS_TARGET_TABLE,
-    )
+    # Check ECS mapping exists
+    filtered_subnets = await filter_ecs_subnets(subnets)
+    logger.info(f"Original number of subnets:: {len(subnets)}")
+    logger.info(f"Remaining subnets to geolocate:: {len(filtered_subnets)}")
 
-    # geoloc
+    # ECS mapping
+    if filtered_subnets:
+        await ecs_mapping(
+            subnets=filtered_subnets,
+            hostnames=hostnames,
+            ecs_table=clickhouse_settings.ECS_TARGET_TABLE,
+        )
+    else:
+        logger.info("Skipping ECS resolution because all subnet mapping is done")
+
+    # Check score
+    filtered_subnet_score = await filter_score_subnets(subnets)
+    logger.info(f"Original number of subnets:: {len(subnets)}")
+    logger.info(f"Score calculation for {len(filtered_subnet_score)} subnets")
+
+    # Calculate scores
+    if filtered_subnet_score:
+        await calculate_scores(filtered_subnet_score, hostnames)
+    else:
+        logger.info(f"Skipping score calculation bacause all score subnet is available")
+
+    # Check pings
+    filtered_targets = filter_targets(targets)
+
+    logger.info(f"Original number of targets:: {len(targets)}")
+    logger.info(f"Pings for {len(filtered_targets)} targets")
+
+    if filtered_targets:
+        # Pings
+        measurement_schedule = get_measurement_schedule(filtered_targets, subnets)
+
+        logger.info(f"Starting geolocation for {len(measurement_schedule)} targets")
+
+        # await ping_targets(measurement_schedule, clickhouse_settings.PING_TARGET_TABLE)
+
+        logger.info("Geolocation done, retrieving measurements")
+    else:
+        logger.info("Skipping pings because they already exists")
+
+    # Create geolocation table/file
+    await insert_geoloc_from_pings(targets)
 
 
 def main(target_file: Path, hostname_file: Path = None):
