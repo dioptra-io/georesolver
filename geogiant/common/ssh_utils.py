b27b7c8d0ed12970c7dbe12aaa04b613c5f6a3e8
hrimlinger <hugorimlinger4@gmail.com>
2024-10-11 13:03:41 +0000

    exit on failure ssh cmd

--- geogiant/common/ssh_utils.py
+++ geogiant/common/ssh_utils.py
@@ -1,5 +1,6 @@
 """functions utilities for excuting shell commands on remote server with fabric"""
 
+from pathlib import Path
 from loguru import logger
 from fabric import Connection, Result
 
@@ -54,7 +55,8 @@ def ssh_run_cmds(
     host: str,
     gateway_user: str = None,
     gateway_host: str = None,
-    hide: bool = False,
+    hide: bool = True,
+    exit_on_failure: bool = False,
 ) -> list[Result]:
     """same as ssh_run_cmd but for a list of cmd"""
     results = []  # list of cmd results
@@ -62,7 +64,7 @@ def ssh_run_cmds(
         with Connection(host=host, user=user) as session:
             for cmd in cmds:
                 result = session.run(cmd, hide=hide)
-                check_error(result)
+                check_error(result, exit_on_failure)
                 results.append(result)
 
     else:
@@ -76,7 +78,41 @@ def ssh_run_cmds(
         ) as session:
             for cmd in cmds:
                 result = session.run(cmd, hide=hide)
-                check_error(result)
+                check_error(result, exit_on_failure)
                 results.append(result)
 
     return results
+
+
+def ssh_upload_file(
+    in_file: Path,
+    out_file: Path,
+    user: str,
+    host: str,
+    gateway_user: str = None,
+    gateway_host: str = None,
+) -> Result:
+
+    # upload hostname file
+    if not gateway_user or not gateway_host:
+        with Connection(host=host, user=user) as session:
+            result = session.put(
+                local=str(in_file),
+                remote=str(out_file),
+            )
+
+    else:
+        with Connection(
+            host=gateway_host,
+            user=gateway_user,
+            gateway=Connection(
+                host=host,
+                user=user,
+            ),
+        ) as session:
+            result = session.put(
+                local=str(in_file),
+                remote=str(out_file),
+            )
+
+    return result
