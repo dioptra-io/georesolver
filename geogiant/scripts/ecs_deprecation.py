5764eaf2304725b8924864494b96ea68a6321bfa
hrimlinger <hugorimlinger4@gmail.com>
2024-10-18 12:06:17 +0000

    fixes ecs deprecation experiment

--- geogiant/scripts/ecs_deprecation.py
+++ geogiant/scripts/ecs_deprecation.py
@@ -1,13 +1,15 @@
 """this script is meant to run periodically to test if the VPs' ECS resolution becomes stale throught time"""
 
+import asyncio
+
 from uuid import uuid4
 from pathlib import Path
 from loguru import logger
-from datetime import datetime
+from pprint import pformat
 
 from geogiant.scheduler import create_agents
 from geogiant.agent import run_dns_mapping, ProcessNames
-from geogiant.clickhouse.queries import load_targets, load_vps, create_tmp_csv_file
+from geogiant.clickhouse.queries import load_targets, load_vps, get_tables
 from geogiant.common.ip_addresses_utils import get_prefix_from_ip
 from geogiant.common.files_utils import load_json, dump_json, dump_csv
 from geogiant.common.settings import PathSettings, ClickhouseSettings
@@ -15,76 +17,157 @@ from geogiant.common.settings import PathSettings, ClickhouseSettings
 path_settings = PathSettings()
 clickhouse_settings = ClickhouseSettings()
 
-CONFIG_PATH = path_settings.DEFAULT / "../experiment_config/config_local_example.json"
+# input files paths
+EXPERIMENT_NAME = "ecs_deprecation"
+CONFIG_PATH = path_settings.DEFAULT / "../experiment_config/ecs_deprecation_config.json"
 TARGET_FILE = path_settings.DATASET / "ripe_atlas_anchors_targets.csv"
 VPS_SUBNET_PATH = path_settings.DATASET / "vps_subnet.csv"
 HOSTNAME_FILE = path_settings.HOSTNAME_FILES / "hostnames_georesolver.csv"
-DEMO_ECS_MAPPING_TABLE = "demo_ecs_mapping"
-NB_ADDRS = 10_000
+
+# clickhouse tables experiments
+VPS_INIT_ECS_TABLE = EXPERIMENT_NAME + "__vps_init_ecs"
+
+
+def print_header(msg: str) -> None:
+    """log header like message"""
+    logger.info("########################################")
+    logger.info(f"# {msg}")
+    logger.info("########################################")
+
+
+def load_datasets(target_file: Path, vps_subnet_file: Path) -> None:
+    """load ripe atlas anchors, vps subnets and output files in defined dirs"""
+    # generate ripe atlas anchors and vps subnet dataset
+    targets = load_targets(clickhouse_settings.VPS_FILTERED_TABLE)
+    targets = [target["addr"] for target in targets][:2]
+    dump_csv(targets, target_file)
+
+    vps = load_vps(clickhouse_settings.VPS_FILTERED_FINAL_TABLE)
+    vps_subnet = [get_prefix_from_ip(v["subnet"]) for v in vps][:3]
+    dump_csv(vps_subnet, vps_subnet_file)
+
+    return targets, vps_subnet
 
 
 def update_config_uuids(config_path: Path) -> str:
     """update the agent uuid of experiment config (to have conflict with older measurements)"""
-    new_experiment_uuid = uuid4()
-    new_agent_uuid = uuid4()
-    config = load_json(config_path)
+    config = load_json(config_path, exit_on_failure=True)
 
-    # save previous uuids in case of failure
+    new_experiment_uuid = str(uuid4())
+    new_agent_uuid = str(uuid4())
+
     # set new agent uuid
-    config["experiment_uuid"] = str(new_experiment_uuid)
-    config["agents"][0]["agent_uuid"] = str(new_agent_uuid)
+    config["experiment_uuid"] = new_experiment_uuid
+    config["agents"][0]["agent_uuid"] = new_agent_uuid
 
-    # dump updated config
+    # dump updated config, ok to override, scheduler create new config
     dump_json(config, config_path)
 
-    return str(new_experiment_uuid)
+    return new_experiment_uuid
 
 
-def update_input_vps_ecs_table(
+def update_in_vps_ecs_table(
     config_path: Path,
     vps_ecs_table: str,
 ) -> None:
     """update score process in config to use the right VPs table"""
     config = load_json(config_path)
 
-    for process, process_def in config["processes"].items():
-        if process == ProcessNames.SCORE_PROC.value:
+    for process_def in config["processes"]:
+        if process_def["name"] == ProcessNames.SCORE_PROC.value:
             process_def["vps_ecs_table"] = vps_ecs_table
 
     dump_json(config, config_path)
 
 
-def main_ecs_deprecation() -> None:
-    """run VPs ECS deprecation experiment"""
-    # 1. load targets (RIPE Atlas anchors)
-    # 2. day 1: perform DNS resolution and georesolver on targets
-    # 3. following days:
-    #       - perform new DNS resolution
-    #       - perform geolocation with new resolution
-    #       - perform geolocation with day 1 resolution
-    # dump N random address in demo file
+def parse_uuid(uuid: str) -> str:
+    """clickhouse only allow underscore for name tables"""
+    return uuid.replace("-", "_")
 
-    # generate ripe atlas anchors and vps subnet dataset
-    targets = load_targets(clickhouse_settings.VPS_FILTERED_TABLE)
-    targets = targets[:2]
-    dump_csv(targets, TARGET_FILE)
 
-    vps = load_vps(clickhouse_settings.VPS_FILTERED_FINAL_TABLE)
-    vps_subnet = [get_prefix_from_ip(v["subnet_v4"]) for v in vps][:3]
-    dump_csv(vps_subnet, VPS_SUBNET_PATH)
+def update_out_target_tables(config_path: Path) -> None:
+    """update score process in config to use the right VPs table"""
+    config = load_json(config_path)
+
+    for process_def in config["processes"]:
+        if process_def["name"] == ProcessNames.ECS_PROC.value:
+            prefix_in = "ecs"
+            prefix_out = "ecs"
+        if process_def["name"] == ProcessNames.SCORE_PROC.value:
+            prefix_in = "ecs"
+            prefix_out = "score"
+        if process_def["name"] == ProcessNames.PING_PROC.value:
+            prefix_in = "score"
+            prefix_out = "ping"
+        if process_def["name"] == ProcessNames.INSERT_PROC.value:
+            prefix_in = "ping"
+            prefix_out = "geoloc"
+
+        process_def["in_table"] = (
+            EXPERIMENT_NAME + f"__{prefix_in}_{parse_uuid(config['experiment_uuid'])}"
+        )
+        process_def["out_table"] = (
+            EXPERIMENT_NAME + f"__{prefix_out}_{parse_uuid(config['experiment_uuid'])}"
+        )
+
+    dump_json(config, config_path)
+
+
+def update_config(
+    experiment_name: str,
+    config_path: Path,
+    vps_ecs_table: str = None,
+) -> dict:
+    """
+    update the experiment config (uuids, vps ecs table and out tables) between each round
+    override is ok because scheduler output newly created config to experiement path
+    """
+    # just in case, update target file
+    config = load_json(config_path)
+    config["target_file"] = str(TARGET_FILE.resolve())
+    dump_json(config, config_path)
 
     # update config
-    update_config_uuids()
+    new_experiement_uuid = update_config_uuids(config_path)
 
-    # run ecs on vps
-    run_dns_mapping(
-        subnets=vps_subnet,
-        hostname_file=HOSTNAME_FILE,
-        output_table="ecs_deprecation__init_vps_ecs",
-    )
+    # create new vps ecs table with experiment uuid
+    if not vps_ecs_table:
+        vps_ecs_table = experiment_name + f"vps_ecs_{new_experiement_uuid}"
 
     # update config so score process use the right vps ecs table
-    update_input_vps_ecs_table(CONFIG_PATH, "ecs_deprecation__init_vps_ecs")
+    update_in_vps_ecs_table(config_path, vps_ecs_table)
+
+    # update target output table
+    update_out_target_tables(config_path)
+
+    return vps_ecs_table
+
+
+def table_exists(table_name: str) -> bool:
+    """check if a table exists"""
+    tables = get_tables()
+
+    if table_name in tables:
+        return True
+
+    return False
+
+
+async def init_experiment(vps_subnet: list[str]) -> None:
+    """step 0: perform ECS with vps and run georesolver"""
+    # create fresh experiment config
+    update_config(
+        experiment_name=EXPERIMENT_NAME,
+        config_path=CONFIG_PATH,
+        vps_ecs_table=VPS_INIT_ECS_TABLE,
+    )
+
+    # run ECS mapping resolution init
+    await run_dns_mapping(
+        subnets=vps_subnet,
+        hostname_file=HOSTNAME_FILE,
+        output_table=VPS_INIT_ECS_TABLE,
+    )
 
     # run georesolver
     agents = create_agents(CONFIG_PATH)
@@ -93,5 +176,78 @@ def main_ecs_deprecation() -> None:
         agent.run()
 
 
+async def run_experiment(vps_subnet: list[str]) -> None:
+    """step 1: run the experiment: 1) ecs vps mapping, 2) georesolver old, 3) georesolver new"""
+
+    print_header("GeoResolver:: old VPs mapping")
+    logger.info(f"Running Georesolver with VPs ECS mapping:: {VPS_INIT_ECS_TABLE}")
+
+    # create fresh experiment config
+    update_config(
+        experiment_name=EXPERIMENT_NAME,
+        config_path=CONFIG_PATH,
+        vps_ecs_table=VPS_INIT_ECS_TABLE,
+    )
+
+    # run georesolver
+    agents = create_agents(CONFIG_PATH)
+    for agent in agents:
+        agent.run()
+
+    new_config = update_config(
+        experiment_name=EXPERIMENT_NAME,
+        config_path=CONFIG_PATH,
+    )
+
+    print_header("GeoResolver:: new VPs mapping")
+    logger.info("Running new experiment round with config::\n{}", pformat(new_config))
+
+    # extract vps ecs table from newly created config
+    vps_ecs_table = [
+        proc_def["vps_ecs_table"]
+        for proc_def in new_config["processes"]
+        if proc_def["name"] == ProcessNames.SCORE_PROC.value
+    ][0]
+
+    logger.info(f"Running Georesolver with new VPs ECS mapping:: {vps_ecs_table}")
+
+    # run ecs on vps
+    await run_dns_mapping(
+        subnets=vps_subnet,
+        hostname_file=HOSTNAME_FILE,
+        output_table=vps_ecs_table,
+    )
+
+    # run georesolver
+    agents = create_agents(CONFIG_PATH)
+    for agent in agents:
+        agent.run()
+
+
+async def main() -> None:
+    """
+    run VPs ECS deprecation experiment
+    1. load targets (RIPE Atlas anchors)
+    2. init   : perform DNS resolution and georesolver on targets
+    3. rounds :
+          - perform new DNS resolution
+          - perform geolocation with new resolution
+          - perform geolocation with day 1 resolution
+    """
+
+    targets, vps_subnet = load_datasets(TARGET_FILE, VPS_SUBNET_PATH)
+
+    print_header("ECS deprecation experiment")
+    logger.info(f"Nb targets     :: {len(targets)}")
+    logger.info(f"Nb VPs subnets :: {len(vps_subnet)}")
+
+    if not table_exists(VPS_INIT_ECS_TABLE):
+        logger.info(f"Running init experiment")
+        await init_experiment(vps_subnet)
+    else:
+        logger.info(f"Running deprecation experiment")
+        await run_experiment(vps_subnet)
+
+
 if __name__ == "__main__":
-    main_ecs_deprecation()
+    asyncio.run(main())
